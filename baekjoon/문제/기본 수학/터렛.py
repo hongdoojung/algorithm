9bff15f628cca610379b736e6bf4be6423becded
Larry Jung <jja02059@gmail.com>
2021-04-19 16:48:01 +0900

    터렛

--- baekjoon/문제/기본 수학/터렛.py
+++ baekjoon/문제/기본 수학/터렛.py
@@ -1,28 +1,42 @@
 def get_length_square(x0, y0, x1, y1):
-    return (x0-x1)**2 + (y0-y1)**2
+    return ((x0 - x1)**2 + (y0 - y1)**2) ** (1/2)
 
 how_many = int(input())
 lines = []
 for i in range(how_many):
     lines.append(input().split())
 
-
 for line in lines:
-    
-    if line[0] == line[3] and line[1] == line[4] and line[2] == line[5]:
-        return -1
+    line = list(map(int,line))
+    if line[2] < line[5]:
+        small_circle = (line[0], line[1], line[2])
+        large_circle = (line[3], line[4], line[5])
+    else:
+        small_circle = (line[3], line[4], line[5])
+        large_circle = (line[0], line[1], line[2])
 
-    # if lines[2] < lines[5]:
-    #     small_circle = (lines[0], lines[1], lines[2])
-    #     large_circle = (lines[3], lines[4], lines[5])
-    # else:
-    #     small_circle = (lines[3], lines[4], lines[5])
-    #     large_circle = (lines[0], lines[1], lines[2])
+    if small_circle == large_circle:
+        print(-1)
+        continue
 
-    # if get_length_square(small_circle[0], small_circle[1], large_circle[0], large_circle[1]) < large_circle[2]:
-
-    # length_square = (line[0]-line[3])**2 + (line[1]-line[4])**2
-    # diameter_sum_square = (line[2]+line[5])**2
-
-    elif length_square < diameter_sum_square:
-
+    length = get_length_square(small_circle[0], small_circle[1], large_circle[0], large_circle[1])
+    if length <= large_circle[2]:
+        if length + small_circle[2] == large_circle[2]:
+            print(1)
+            continue
+        elif length + small_circle[2] < large_circle[2]:
+            print(0)
+            continue
+        else:
+            print(2)
+            continue
+    else:
+        if length == large_circle[2] + small_circle[2]:
+            print(1)
+            continue
+        if length > large_circle[2] + small_circle[2]:
+            print(0)
+            continue
+        else:
+            print(2)
+            continue
