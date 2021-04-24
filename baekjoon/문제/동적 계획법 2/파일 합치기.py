bcd3b4322a916cea59c3509da126762eee7fa218
Larry Jung <jja02059@gmail.com>
2021-04-24 20:18:28 +0900

    파일 합치기 (이해못함)

--- baekjoon/문제/동적 계획법 2/파일 합치기.py
+++ baekjoon/문제/동적 계획법 2/파일 합치기.py
@@ -6,13 +6,19 @@ for i in range(how_many):
     line = list(map(int, input().split()))
     lines.append(line)
 
-for line in lines:
-    dp = list(list(0 for _ in range(len(line))) for _ in range(len(line)))
-    for k in range(1,len(line)):
-        for i in range(len(line)-k):
-            x, y = i, i+k
-            for j in range(k):
-                tmp = dp[x + 1 + j][y]+dp[x][y - k + j]
-                dp[x][y] = min(dp[x][y],tmp) if dp[x][y]>0 else tmp
-            dp[x][y] += sum(line[x : y + 1])
-    print(dp[0][-1])
+for line in lines: 
+    sum = [line[0]]
+    for i in line[1:]: 
+        sum.append(i + sum[-1]) 
+    accumulated = [[0] * len(line) for _ in range(len(line))]
+    
+    accumulated[0][1] = sum[1] 
+    for i in range(1, len(line) - 1):
+        accumulated[i][i + 1] = sum[i + 1] - sum[i - 1] 
+    for gap in range(2, len(line)): 
+        for i in range(len(line) - gap): 
+            accumulated[i][i + gap] = float('inf') 
+            for j in range(i, i + gap): 
+                accumulated[i][i + gap] = min(accumulated[i][i + gap], accumulated[i][j] + accumulated[j + 1][i + gap]) 
+            accumulated[i][i + gap] = accumulated[i][i + gap] + sum[i + gap] - sum[i - 1] if i > 0 else accumulated[0][gap] + sum[gap]
+    print(accumulated[0][len(line) - 1])
