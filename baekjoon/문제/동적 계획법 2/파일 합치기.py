25c852af2faa6fa9626cc52275c600863ec2ffad
Larry Jung <jja02059@gmail.com>
2021-05-01 11:46:35 +0900

    파일 합치기

--- baekjoon/문제/동적 계획법 2/파일 합치기.py
+++ baekjoon/문제/동적 계획법 2/파일 합치기.py
@@ -1,28 +1,35 @@
-#이해안됨
-how_many = int(input())
-lines = []
-for i in range(how_many):
-    input()
-    line = list(map(int, input().split()))
-    lines.append(line)
+def collect_inputs():
+    how_many = int(input())
+    lines = []
+    for i in range(how_many):
+        input()
+        line = list(map(int, input().split()))
+        lines.append(line)
+    return lines
 
-for line in lines:
+def calculate_min_sum(line):
     length = len(line)
-    max_int = sum(line) * length
-    dp = [[0]*(length+1) for i in range(length+1)]
     costs = [line[0]]
+    for i in range(1, len(line)):
+        costs.append(costs[i-1] + line[i])
 
-    for i in range(1, length):
-        costs.append(costs[i-1]+line[i])
-    costs.append(0)
+    dp = [[0]*(length) for _ in range(length)]
+    for i in range(length - 1):
+        dp[i][i+1] = line[i] + line[i+1]
 
-    for gap in range(1, length):
-        for start in range(length):
-            end = start + gap
-            if end == length:
-                break
-            dp[start][end] = max_int
-            for i in range(start, end):
-                candidate = dp[start][i] + dp[i+1][end] + (costs[end]-costs[start-1])
-                dp[start][end] = min(dp[start][end], candidate)
-    print(dp[0][length-1])
+    for j in range(2, length):
+        i = 0
+        while i + j < length:
+            for k in range(i, i+j):
+                cost_dist = costs[i+j] - costs[i-1] if i!=0 else costs[i + j]
+                if dp[i][i+j] == 0:
+                    dp[i][i+j] = dp[i][k] + dp[k+1][i+j] + cost_dist
+                else:
+                    dp[i][i+j] = min(dp[i][i+j], dp[i][k] + dp[k+1][i+j] + cost_dist)
+            i += 1
+    return dp[0][-1]
+    
+if __name__ == "__main__":
+    lines = collect_inputs()
+    for line in lines:
+        print(calculate_min_sum(line))
