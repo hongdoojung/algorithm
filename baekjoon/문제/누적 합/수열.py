fe2c27b14a023eaf5044e8a8c3d5458e5ae75264
Larry Jung <jja02059@gmail.com>
2023-05-14 00:08:05 +0900

    Create 수열.py

---
+++ baekjoon/문제/누적 합/수열.py
@@ -0,0 +1,21 @@
+# 입력
+# 첫째 줄에는 두 개의 정수 N과 K가 한 개의 공백을 사이에 두고 순서대로 주어진다. 첫 번째 정수 N은 온도를 측정한 전체 날짜의 수이다. N은 2 이상 100,000 이하이다. 두 번째 정수 K는 합을 구하기 위한 연속적인 날짜의 수이다. K는 1과 N 사이의 정수이다. 둘째 줄에는 매일 측정한 온도를 나타내는 N개의 정수가 빈칸을 사이에 두고 주어진다. 이 수들은 모두 -100 이상 100 이하이다. 
+
+# 출력
+# 첫째 줄에는 입력되는 온도의 수열에서 연속적인 K일의 온도의 합이 최대가 되는 값을 출력한다.
+
+
+def main():
+    length, part_length = (map(int, input().split()))
+    array = list(map(int, input().split()))
+
+    part_sum = sum(array[0:part_length])
+    max_part_sum = part_sum
+    for i in range(length - part_length):
+        part_sum = part_sum - array[i] + array[i + part_length]
+        max_part_sum = max(max_part_sum, part_sum)
+    print(max_part_sum)
+
+
+if __name__ == "__main__":
+    main()
