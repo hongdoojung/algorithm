4455bff008389d309eade4e1434abda5744ca46d
Larry Jung <jja02059@gmail.com>
2023-05-31 19:43:39 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 2336 ms, Memory: 85284 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,43 @@
+import sys
+import heapq
+
+sys.setrecursionlimit(10**6)
+
+
+def dfs(R, E, V, indexs):
+    # print("called", R, V, indexs)
+    V[R] = indexs.pop(0)
+    heap = []
+    for v in E[R]:
+        if not V.get(v):
+            heapq.heappush(heap, -1 * v)
+    while heap:
+        min_value = -1 * heapq.heappop(heap)
+        if not V.get(min_value):
+            dfs(min_value, E, V, indexs)
+
+
+# dfs(V, E, R) {  # V : 정점 집합, E : 간선 집합, R : 시작 정점
+#     visited[R] <- YES;  # 시작 정점 R을 방문 했다고 표시한다.
+#     for each x ∈ E(R)  # E(R) : 정점 R의 인접 정점 집합.(정점 번호를 오름차순으로 방문한다)
+#         if (visited[x] = NO) then dfs(V, E, x);
+# }
+
+def main():
+    N, M, R = list(map(int, sys.stdin.readline().split()))
+
+    E = [[] for _ in range(N+1)]
+    V = dict()
+    for i in range(M):
+        a, b = list(map(int, sys.stdin.readline().split()))
+        E[a].append(b)
+        E[b].append(a)
+
+    indexs = [i for i in range(1, N + 1)]
+    dfs(R, E, V, indexs)
+    for i in range(1, N + 1):
+        print(V.get(i, 0))
+
+
+if __name__ == "__main__":
+    main()
